2d84ad28b858e2bec1c40c86dfb0b03c1f549fdc
kyusieun <kyusieun@gmail.com>
2023-05-16 12:50:29 +0900

    OAuth 추가

---
+++ src/components/OAuth.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <p>Received authorization code: {{ $route.params.code }}</p>
+    <button @click="sendCode">Send code to backend</button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["code"],
+  methods: {
+    sendCode() {
+      // Make an HTTP request to your backend API with the code
+      // For example, using Axios:
+      axios
+        .post("/api/auth/kakao", { code: this.code })
+        .then((response) => {
+          // Handle successful response
+        })
+        .catch((error) => {
+          // Handle error
+        });
+    },
+  },
+};
+</script>
