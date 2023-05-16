581df5b6bd55b9d9f34149caf8112f089c2223cf
kyusieun <kyusieun@gmail.com>
2023-05-16 14:31:05 +0900

    OAuth 수정 간단한 코드표시

--- src/components/OAuth.vue
+++ src/components/OAuth.vue
@@ -1,27 +1,15 @@
 <template>
   <div>
-    <p>Received authorization code: {{ $route.params.code }}</p>
-    <button @click="sendCode">Send code to backend</button>
+    <p>Received authorization code: {{ code }}</p>
   </div>
 </template>
 
 <script>
-// import axios from "axios";
 export default {
-  props: ["code"],
-  // methods: {
-  //   sendCode() {
-  //     // Make an HTTP request to your backend API with the code
-  //     // For example, using Axios:
-  //     axios
-  //       .post("/api/auth/kakao", { code: this.code })
-  //       .then((response) => {
-
-  //       })
-  //       .catch((error) => {
-
-  //       });
-  //   },
-  // },
+  computed: {
+    code() {
+      return this.$route.query.code;
+    },
+  },
 };
 </script>
